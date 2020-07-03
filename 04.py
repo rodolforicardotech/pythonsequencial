f1621a7c9414f03693455ac8970ede65c3d5cad9
rodolforicardo7 <rodolforicardotech@gmail.com>
2020-07-02 21:11:19 -0300

    add new files

---
+++ 04.py
@@ -0,0 +1,6 @@
+#4) Calcule e apresente a média entre os números 8, 9 e 7.
+x = 8; y = 9; z = 7
+media = (x+y+z)/3
+print('A média entre os três números é:', media, '\n')
+
+# O '\n' também quebra linha no Python
